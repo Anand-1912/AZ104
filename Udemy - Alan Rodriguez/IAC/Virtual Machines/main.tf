d5a9c740cfca1d24d3e68d6e6a043b4d3f805842
Anand-1912 <34396688+Anand-1912@users.noreply.github.com>
2024-01-28 23:44:26 +0000

    add Virtual Machine (#2)
    add VNet, SubNet, NIC and Virtual Machine Resource Blocks

--- Udemy - Alan Rodriguez/IAC/Virtual Machines/main.tf
+++ Udemy - Alan Rodriguez/IAC/Virtual Machines/main.tf
@@ -1,7 +1,6 @@
 
 # Terraform Block
 terraform {
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -20,8 +19,62 @@ provider "azurerm" {
   }
 }
 
-// Resource Block and the configuration of the resources
-resource "azurerm_resource_group" "rg_az104" {
-  name     = "rgAZ104"
-  location = "Uk South"
+# Resource Block and the configuration of the resources
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.suffix}-001"
+  location = var.location
 }
+
+# VNet
+resource "azurerm_virtual_network" "main" {
+  name = "vnet-${var.suffix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  address_space =["10.0.0.0/16"]
+}
+
+# Subnet
+
+resource "azurerm_subnet" "internal" {
+  name = "snet-${var.suffix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes = ["10.0.0.0/24"]
+}
+
+
+# NIC
+
+resource "azurerm_network_interface" "main" {
+  name = "nic-${var.suffix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  ip_configuration {
+    name = "internal"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id = azurerm_subnet.internal.id
+  }
+}
+
+# VM
+resource "azurerm_windows_virtual_machine" "main" {
+  name = "vm-${var.suffix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  network_interface_ids = [azurerm_network_interface.main.id]
+  admin_username = "adminuser"
+  admin_password = "P@ssw0rd1234!"
+  size ="Standard_B1s"
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+} 
